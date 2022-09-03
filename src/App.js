a8378ecf4bf4616d2d2ee256995207e0a6f9bb31
Saugat Dhungel <saugat.dhungel@hpe.com>
2022-09-03 13:59:08 -0400

    todos done functionality

--- src/App.js
+++ src/App.js
@@ -1,7 +1,44 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
-  return <div className='App'></div>;
+  const [todos, setTodos] = useState([
+    { item: 'Take a jog', done: false, id: 1 },
+    { item: 'Go to work', done: true, id: 2 },
+    { item: 'Do homework', done: false, id: 3 },
+  ]);
+
+  // const completeTodo = index => {
+  //   const newTodos = [...todos];
+  //   newTodos[index].isCompleted = true;
+  //   setTodos(newTodos);
+  // };
+
+  const handleClickDone = (index) => {
+    const newTodos = [...todos];
+    console.log(newTodos);
+    newTodos[index].done = true;
+    setTodos(newTodos);
+  };
+
+  return (
+    <div className='App'>
+      <ul>
+        {todos.map((todo, index) => {
+          return (
+            <div style={{ display: 'flex' }}>
+              <li
+                style={{ textDecoration: todo.done ? 'line-through' : 'none' }}
+              >
+                {todo.item}
+              </li>
+              <button onClick={() => handleClickDone(index)}>Done</button>
+            </div>
+          );
+        })}
+      </ul>
+    </div>
+  );
 }
 
 export default App;
